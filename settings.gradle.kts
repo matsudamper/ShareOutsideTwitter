25207fff9e8fa55573aa7d8557de7594b2860fca
matsudamper <let@matsudamper.net>
2023-07-10 08:14:10 +0900

    version code 3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("libs") {
-            version("versionCode", "2")
+            version("versionCode", "3")
             version("versionName", "1.0.0")
 
             version("android.sdk.min", "24")
